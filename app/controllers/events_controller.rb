5e5cf7f40fa8402e1612f173888643f4c87e0ad4
AlexChi <alexchi1114@gmail.com>
2017-06-06 14:44:59 -0400

    Added ability to see upcoming and previous events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,13 +7,17 @@ class EventsController < ApplicationController
 		@event = current_user.events.build(event_params)
 		@event.date = Date.strptime(@event.event_date, '%m/%d/%Y')
 		if @event.save
+			
 			redirect_to events_path
+		else
+			render 'new'
 		end
 	end
 
 	def index
-		@events = Event.all.order(created_at: :desc)
-		
+		#@events = Event.all.order(created_at: :desc)
+		@upcoming_events = Event.upcoming
+		@previous_events = Event.past
 	end
 
 	def show
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,10 @@ class Event < ApplicationRecord
 	belongs_to :creator, :class_name => "User"
 	has_many :invites, :foreign_key => :attended_event_id
 	has_many :attendees, through: :invites
+	
+	validates :event_date, presence: true, format: {with: /\d{2}\/\d{2}\/\d{4}/}
 	#date = Date.strptime(@event_date,'%m/%d/%Y')
-	#scope :upcoming, -> {where('Date.strptime(event_date, '%m/%d/%Y') >= ?', DateTime.now)}
-	#scope  :past,  -> { where('date < ?', DateTime.now)}
-
+	scope :upcoming, -> {where('date >= ?', DateTime.now)}
+	scope  :past,  -> { where('date < ?', DateTime.now)}
 		
 end
